adabc4a6e218fe56e84fd323e6e40881ee539473
Max <maximpalaguta@gmail.com>
2021-03-05 12:59:14 +0200

    add mask

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,6 @@ $(document).ready(function () {
     validateForms('#consultation form');
     validateForms('#consultation-form');
     validateForms('#order form');
+
+    $('input[name=phone]').mask("+38 (999) 999-99-99");
 });
